7492484c5b2d153aa9a91e20ab7367243e26f848
Henrietha <areathy33@gmail.com>
2021-10-15 13:58:49 +0100

    Badge

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -5,9 +5,12 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatExpansionModule } from "@angular/material/expansion";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatDividerModule } from "@angular/material/divider";
 
 let materialModules = [
-  MatButtonModule, MatInputModule, MatToolbarModule, MatIconModule, MatMenuModule, MatExpansionModule
+  MatButtonModule, MatInputModule, MatToolbarModule, MatIconModule, MatMenuModule,
+  MatExpansionModule, MatBadgeModule, MatDividerModule
 ];
 
 @NgModule({
