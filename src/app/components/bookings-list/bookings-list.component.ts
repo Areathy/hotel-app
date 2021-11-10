2fe276536c280c86e1106f5b4d6e44773c0ea9be
Henrietha <areathy33@gmail.com>
2021-11-10 23:28:37 +0100

    Further code for bookings List Table

--- src/app/components/bookings-list/bookings-list.component.ts
+++ src/app/components/bookings-list/bookings-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from "@angular/material/table";
+import { FormGroup, FormControl } from "@angular/forms";
 
 import { Booking } from "../../models/booking";
 import { BookingsService } from "../../services/bookings.service";
@@ -16,8 +17,13 @@ export class BookingsListComponent implements OnInit {
   isLoadingCompleted: boolean = false;
   isError: boolean = false;
   rows: Booking[] = [];
+  formGroup!: FormGroup;
 
-  constructor(private bookingsService: BookingsService) { }
+  constructor(private bookingsService: BookingsService) {
+    this.formGroup = new FormGroup({
+      search: new FormControl(null)
+    });
+  }
 
   ngOnInit(): void {
     this.isLoadingCompleted = false;
@@ -26,8 +32,16 @@ export class BookingsListComponent implements OnInit {
       (response: Booking[]) => {
         this.bookings = new MatTableDataSource<Booking>(response);
         this.rows = response;
-        
+
         this.isLoadingCompleted = true;
+
+        //filterPredicate
+        this.bookings.filterPredicate = (data, filter) => {
+          return this.columnsToDisplay.some((column, i) => {
+            return column != "actions" && column != "selection" && (data as any)[column] && (data as any)[column]
+              .toString().toLowerCase().indexOf(filter) != -1;
+          })
+        }
       },
       (error) => {
         console.log(error);
@@ -35,6 +49,13 @@ export class BookingsListComponent implements OnInit {
         this.bookingLoadingStatus = "Error fetching the data";
       },
     )
+  }
+
+  //Executes when the user changes the value of search textbox
+  filterBookings() {
+    if (this.formGroup.value.search != null && this.bookings) {
+      this.bookings.filter = this.formGroup.value.search.trim();
+    }
   }
 
 }
