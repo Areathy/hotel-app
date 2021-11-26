0d5cee05cf84ed44e4b74590326219451c53bc21
Henrietha <areathy33@gmail.com>
2021-11-26 14:24:00 +0100

    Implemented the close event for the dialog box

--- src/app/components/change-dates/change-dates.component.ts
+++ src/app/components/change-dates/change-dates.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { Booking } from 'src/app/models/booking';
 
 @Component({
@@ -12,7 +12,7 @@ export class ChangeDatesComponent implements OnInit {
 
   formGroup: FormGroup;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public dialogData: Booking) { 
+  constructor(@Inject(MAT_DIALOG_DATA) public dialogData: Booking, public matDialogRef: MatDialogRef<ChangeDatesComponent>) { 
     this.formGroup = new FormGroup({
       checkIn: new FormControl(null),
       checkOut: new FormControl(null)
@@ -27,4 +27,10 @@ export class ChangeDatesComponent implements OnInit {
     });
   }
 
+  //Executes when the user clicks on Cancel / Close button
+  onCancelClick() {
+    this.matDialogRef.close();
+  }
+
 }
+ 
