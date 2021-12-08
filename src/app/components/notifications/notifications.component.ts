f2cab1cb8097cf2960b70089086e45e2bd45f1e2
Henrietha <areathy33@gmail.com>
2021-12-08 20:47:04 +0100

    "Okay" button closing the snack bar

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_SNACK_BAR_DATA } from "@angular/material/snack-bar";
+import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-notifications',
@@ -10,7 +10,7 @@ export class NotificationsComponent implements OnInit {
 
   textCssClass: string = "";
 
-  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any) { }
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any, public snackBarRef: MatSnackBarRef<NotificationsComponent>) { }
 
   ngOnInit(): void {
     this.textCssClass = this.data.messageType == "success" ? 'text-green' : (this.data.messageType == "error" ? 'text-red' : 'text-black');
